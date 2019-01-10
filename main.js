50d37463f55db5951669165f7fbf5f4cf7bd41ce
Frederik Kirk <kaae-2@hotmail.com>
2019-01-10 18:06:00 +0100

    format main.js

--- main.js
+++ main.js
@@ -1,13 +1,41 @@
-const electron = require('electron')
+const electron = require("electron");
 
-const { app, BrowserWindow } = electron
+const path = require("path");
+const our = require("url");
 
-function createWindow () {
+const { app, BrowserWindow } = electron;
+
+let mainWindow;
+
+function createWindow() {
   // Create the browser window.
-  win = new BrowserWindow({ width: 800, height: 600 })
+  mainWindow = new BrowserWindow({ width: 800, height: 600 });
 
   // and load the index.html of the app.
-  win.loadFile('index.html')
+  mainWindow.loadURL(
+    url.format({
+      pathname: path.join(__dirname, "index.html"),
+      protocol: "file",
+      slashes: true
+    })
+  );
+
+  mainWindow.webContents.openDevTools();
+  mainWindow.on("closed", () => {
+    mainWindow = null;
+  });
 }
 
-app.on('ready', createWindow)
+app.on("ready", createWindow);
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
