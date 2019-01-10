b532f437b867ed37d740e775f81fc94854571aad
Frederik Kirk <kaae-2@hotmail.com>
2019-01-10 18:28:54 +0100

    clean up

---
+++ gui/main.js
@@ -0,0 +1,41 @@
+const electron = require("electron");
+
+const path = require("path");
+const our = require("url");
+
+const { app, BrowserWindow } = electron;
+
+let mainWindow;
+
+function createWindow() {
+  // Create the browser window.
+  mainWindow = new BrowserWindow({ width: 800, height: 600 });
+
+  // and load the index.html of the app.
+  mainWindow.loadURL(
+    url.format({
+      pathname: path.join(__dirname, "index.html"),
+      protocol: "file",
+      slashes: true
+    })
+  );
+
+  mainWindow.webContents.openDevTools();
+  mainWindow.on("closed", () => {
+    mainWindow = null;
+  });
+}
+
+app.on("ready", createWindow);
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
